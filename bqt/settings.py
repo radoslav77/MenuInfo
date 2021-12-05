ec87159945f9935ffaaca58ad24803bcc8dbc8ce
radoslav77 <rado7711@yahoo.co.uk>
2021-12-05 17:07:39 +0000

    #0126

--- bqt/settings.py
+++ bqt/settings.py
@@ -80,8 +80,37 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+'''
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/bqtinfo:us-central1:bqt-info-web-2021',
+            'USER': 'qaz',
+            'PASSWORD': '123',
+            'NAME': 'bqtInfoWeb',
+        }
+    }
+else:
+    # Running locally so connect to either a local MySQL instance or connect
+    # to Cloud SQL via the proxy.  To start the proxy via command line:
+    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'bqtInfoWeb',
+            'USER': 'qaz',
+            'PASSWORD': '123',
+        }
+    }
+# [END db_setup]
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
